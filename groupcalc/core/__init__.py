a19ca7f879c6922de5e6a252b43b62faf3c60f2d
DerVogel101 <jan.irmer@outlook.de>
2025-01-23 00:12:11 +0100

    init

---
+++ groupcalc/core/__init__.py
@@ -0,0 +1,5 @@
+# groupcalc/core/__init__.py
+
+from .logger import CustomLogger, ColoredLogger
+
+__all__ = ['CustomLogger', 'ColoredLogger']
---
+++ groupcalc/core/exceptions.py
@@ -0,0 +1,8 @@
+class GroupCalcError(Exception):
+    pass
+
+class AmountPeopleError(GroupCalcError):
+    pass
+
+class GroupSizeError(GroupCalcError):
+    pass
